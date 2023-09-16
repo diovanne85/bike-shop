352f3f941d9b03a9ab41605a820513e24fddd7f1
Diovanne <dpsagosoy85@gmail.com>
2023-09-17 00:47:58 +0800

    CHANGE

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Poppins': ['Poppins, sans-serif'],
+        textIndent: {
+          '128': '32rem',
+        }
+      },
+      backgroundImage: {
+        'hero-pattern': "url('./image/pexels-pavel-danilyuk-5790072 (1080p).mp4')",
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(90deg)' },
+        }
+      },
+        gridAutoColumns: {
+          'auto-fit': 'repeat(autofit, minmax(250px, 1fr))',
+        }
+
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
